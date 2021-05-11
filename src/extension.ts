771094d5bec4babff9eeab95cb5f30eb4c1807e0
Anton <62949848+TheIcepack@users.noreply.github.com>
2021-05-10 20:12:29 -0400

    Initial Commit

---
+++ src/extension.ts
@@ -0,0 +1,61 @@
+import * as vscode from 'vscode';
+
+let firstRun = true;
+let cursor = {
+	line: 0,
+	character: 0
+};
+
+
+function checkCursor() {
+
+	console.log('checkCursor');
+	let editor = vscode.window.activeTextEditor;
+
+	if (editor) {
+		let active = editor.selection.active;
+		let newCursor = {
+			line: active.line,
+			character: active.line
+		};
+
+		// REF INIT
+		if (firstRun) {
+			firstRun = false;
+			cursor = newCursor;
+			return;
+		}
+
+		// See if the cursor is at a new location
+		if (cursor.line !== active.line && cursor.character !== active.character) {
+			vscode.window.showInformationMessage('Reset the timer when you come back.');
+			vscode.window.showInformationMessage('Get some exercise.');
+			vscode.window.showInformationMessage('It is time to take a break.');
+			
+		}
+		cursor = newCursor;
+	}
+}
+
+export function activate(context: vscode.ExtensionContext) {
+
+	let intervalLength = 1800000;
+	let interval = setInterval(checkCursor, intervalLength); // Check activity will be called every 30 minutes
+	checkCursor(); // Initialize the project. See REF INIT
+
+	let disposable = vscode.commands.registerCommand('takeabreak.resetTimer', () => {
+
+		clearInterval(interval);
+		interval = setInterval(checkCursor, intervalLength);
+
+		vscode.window.showInformationMessage('Timer Reset.');
+	});
+
+	context.subscriptions.push(disposable);
+
+	vscode.window.showInformationMessage('Take a break set for 30 minutes.');
+	vscode.window.showInformationMessage('To reset the timer: Ctrl+Shift+P: Reset Timer');
+}
+
+
+export function deactivate() { }
