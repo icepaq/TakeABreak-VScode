6fccb58820e1fd1adfe96a6c3060253c6498e5c4
Anton <62949848+TheIcepack@users.noreply.github.com>
2021-07-18 19:05:54 -0400

    User can set interval of break alerts

--- src/extension.ts
+++ src/extension.ts
@@ -8,13 +8,24 @@ let cursor = {
 };
 
 
-
 export function activate(context: vscode.ExtensionContext) {
+
+    console.log("Extension Activated");
+    // Parse settings.json
+    let interval = vscode.workspace.getConfiguration().get('takeabreak.interval');
+    let afkreset = vscode.workspace.getConfiguration().get('takeabreak.afkreset');
 
     setInterval(timers.logActivity, 6000); // Log activity
     timers.logActivity();
 
-    setInterval(timers.checkAFK, 60000);
+    if (typeof interval === 'number') {
+        console.log("Settings is a number: " + interval);
+        setInterval(timers.checkAFK, interval * 60000);
+    } else {
+        console.log("Invalid interval setting");
+        setInterval(timers.checkAFK, 1800000);
+    }
+    
     timers.checkAFK();
 
     setInterval(timers.checkTimer, 10000);
@@ -23,6 +34,8 @@ export function activate(context: vscode.ExtensionContext) {
         timers.timerStart = Date.now();
         vscode.window.showInformationMessage('Timer Reset.');
     });
+
+    
 
     context.subscriptions.push(disposable);
 
