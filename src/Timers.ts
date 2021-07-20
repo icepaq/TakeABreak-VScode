49317c10c42b88f1cb783ceaa7181e8f151621b1
Anton <62949848+TheIcepack@users.noreply.github.com>
2021-07-20 15:46:07 -0400

    Commenting

--- src/Timers.ts
+++ src/Timers.ts
@@ -7,6 +7,7 @@ let timerOn = false;
 let timerStart : number;
 
 
+// If the timer is on and has been running for more than the interval, notify the user
 export function checkTimer() {
     if(timerOn && Date.now() - timerStart > 1200000) {
         vscode.window.showInformationMessage('Time to take a break! Reset the timer when you get back!');
@@ -20,14 +21,18 @@ export function startTimer() {
 }
 
 export function logActivity() {
+
+    // If the user is not editing a file, then we don't want to log their activity
     if (!vscode.window.activeTextEditor) {
         return;
     }
+
     const cursor = {
         line: vscode.window.activeTextEditor.selection.active.line,
         character: vscode.window.activeTextEditor.selection.active.character
     };
 
+    // If the person is not afk and the timer is not on, start the timer
     if(!afk && !timerOn) {
         startTimer();
     }
@@ -35,6 +40,8 @@ export function logActivity() {
     cursorHistory.push(cursor);
 }
 
+
+// Check if the user is AFK and if so, reset the timer.
 export function checkAFK() {
 
     console.log('Checking AFK');
